8e6d33a089ddec396645553d4935e2709a937423
Divenero <artemmartus2012@gmail.com>
2019-10-15 18:51:46 +0300

    Initial Commit

---
+++ Json Api Check/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Json Api Check
+//
+//  Created by Artem Martus on 15.10.2019.
+//  Copyright © 2019 Artem Martus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Json Api CheckTests/Json_Api_CheckTests.swift
@@ -0,0 +1,34 @@
+//
+//  Json_Api_CheckTests.swift
+//  Json Api CheckTests
+//
+//  Created by Artem Martus on 15.10.2019.
+//  Copyright © 2019 Artem Martus. All rights reserved.
+//
+
+import XCTest
+@testable import Json_Api_Check
+
+class Json_Api_CheckTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
